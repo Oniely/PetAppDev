a61af73b0984e9d304d8567f911c919f7dc5e26e
Oniely <jabagatniel09@gmail.com>
2024-04-07 15:34:00 +0800

    svg support, fix bug

--- client/App/Screens/LoginScreen/Onboarding.jsx
+++ client/App/Screens/LoginScreen/Onboarding.jsx
@@ -1,25 +1,31 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
+import { useFonts } from 'expo-font';
 import {
-	useFonts,
 	Poppins_400Regular,
 	Poppins_500Medium,
 	Poppins_600SemiBold,
+	Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
+import CheckIcon from '../../../assets/images/check.svg';
 
 const Onboarding = () => {
 	const [fontsLoaded] = useFonts({
 		Poppins_400Regular,
 		Poppins_500Medium,
 		Poppins_600SemiBold,
+		Poppins_700Bold
 	});
 
 	return (
 		<View className="h-full px-4 pt-12 bg-orange-white">
-			<View>
+			<View className="flex-row justify-between">
 				<Image
 					source={require("../../../assets/images/logo.png")}
 					className="object-contain w-10 h-10"
 				/>
+				<View className="bg-main-orange items-center justify-center rounded-full w-10 h-10 p-2">
+					<CheckIcon stroke={"#f1f1f1"} />
+				</View>
 			</View>
 
 			<View>
@@ -31,13 +37,13 @@ const Onboarding = () => {
 
 			<View>
 				<Text
-					style={{ fontFamily: "Poppins_600SemiBold" }}
+					style={{ fontFamily: "Poppins_700Bold" }}
 					className="text-6xl leading-[70px] text-main-orange"
 				>
 					Find Caring
 				</Text>
 				<Text
-					style={{ fontFamily: "Poppins_600SemiBold" }}
+					style={{ fontFamily: "Poppins_700Bold" }}
 					className="text-6xl text-main-orange"
 				>
 					Hands
@@ -47,7 +53,7 @@ const Onboarding = () => {
 			<View>
 				<Text
 					style={{ fontFamily: "Poppins_400Regular" }}
-					className="text-base text-gray-500 mt-1"
+					className="text-base text-gray-500 mt-2 "
 				>
 					Compassionate care for your cherished companions. Feel at
 					ease with our reliable, affectionate services. Your pet’s
@@ -70,4 +76,3 @@ const Onboarding = () => {
 };
 
 export default Onboarding;
-s
