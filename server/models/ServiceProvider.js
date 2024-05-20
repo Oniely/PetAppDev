a46e72aa19d421029dc23e7ac0c70cf2fffff9d8
Oniely <jabagatniel09@gmail.com>
2024-05-20 23:54:18 +0800

    sync the service model from admin

--- server/models/ServiceProvider.js
+++ server/models/ServiceProvider.js
@@ -1,4 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const timeFormat12Hour = /^(0[1-9]|1[0-2]):([0-5][0-9]) ?([AaPp][Mm])$/;
+
+const AvailabilitySchema = new mongoose.Schema(
+	{
+		startTime: {
+			type: String,
+			required: true,
+			match: [
+				timeFormat12Hour,
+				"Start time must be in the format HH:mm AM/PM",
+			],
+		},
+		endTime: {
+			type: String,
+			required: true,
+			match: [
+				timeFormat12Hour,
+				"End time must be in the format HH:mm AM/PM",
+			],
+		},
+	},
+	{ _id: false }
+);
 
 const ServiceProviderSchema = new mongoose.Schema({
 	userId: {
@@ -6,7 +30,10 @@ const ServiceProviderSchema = new mongoose.Schema({
 		required: true,
 		unique: true,
 	},
-	image_url: String,
+	image_url: {
+		type: String,
+		required: true,
+	},
 	phoneNumber: String,
 	companyName: {
 		type: String,
@@ -26,15 +53,21 @@ const ServiceProviderSchema = new mongoose.Schema({
 		type: Boolean,
 		default: false,
 	},
+	operatingHours: {
+		type: AvailabilitySchema,
+		required: true,
+	},
 	servicesOffered: [{ type: mongoose.Schema.Types.ObjectId, ref: "Service" }],
 	ratings: [{ rating: Number, comment: String }],
 });
 
-const Provider = mongoose.models.Provider || mongoose.model("Provider", ServiceProviderSchema);
+const Provider =
+	mongoose.models.Provider ||
+	mongoose.model("Provider", ServiceProviderSchema);
 
 const Service = mongoose.models.Service;
 
-module.exports = { 
+module.exports = {
 	Provider,
-	Service
+	Service,
 };
